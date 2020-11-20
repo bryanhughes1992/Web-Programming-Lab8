68cc3fdea0a0d9d4937cb38fc52cab377ae80c16
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-11-20 01:48:00 -0500

    Rough Copy

--- Lab-8-forms.js
+++ Lab-8-forms.js
@@ -12,22 +12,45 @@ var shipInfo = {
 window.addEventListener("load", firePage);
 
 function firePage() {
+
 	//Storing entire form as a variable
 	var formHandle = document.forms.form_ship;
+
 	//Storing the 'Thank you' msg as a variable
 	var thanksMsg = document.getElementById('thanks_msg');
 
-
-
+	//When the form_ship form is submitted, use the processForm() function.
 	formHandle.onsubmit = processForm;
 
+	//Do the following when this function is called
 	function processForm() {
+
 		//Alert user that the form has sent
 		alert("Form Sent!");
+
+		//Variable created to store user 'Name'
+		var userName = formHandle.in_Name.value;
+		//Setting the 'name' property of the 'shipInfo' object to the 'userName'
+		shipInfo.name = userName;
+
+		//Variable to hold the value of the client id input
+		var client_id = formHandle.in_Id.value;
+		//Setting the 'cid' member to user data
+		shipInfo.cid = client_id;
+
+		//Variable to hold user inputted postal code
+		var postalCode = formHandle.in_pc.value;
+		//Setting the 'pc' member of the 'shipInfo' object to 'postalCode'.
+		shipInfo.pc = postalCode;
+
+		var thankYouMsg = `Thank you ${shipInfo.name} (Customer #${shipInfo.cid}) for your purchase. Your package will be sent to ${shipInfo.postalCode}`;
+
 		//onSubmit, hide the form.
 		formHandle.style.display = 'none';
 		//Display hard coded thank you message
-		thanksMsg.style.display = 'inline-block';
+		thanksMsg.style.display = 'inline';
+
+
 		//Stop the form from sending
 		return false;
 
