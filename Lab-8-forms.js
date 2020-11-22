b784fb79935dcf84eee6f958e96cc2ae09e057e9
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-11-21 22:43:54 -0500

    Final Copy

--- Lab-8-forms.js
+++ Lab-8-forms.js
@@ -34,42 +34,66 @@ function firePage() {
 			DROPDOWN HANDLING
 		*************************/
 
-		var dropDown = formHandle.in_Speed;
+		var levelOfDelivery = formHandle.in_Speed;
 
-		dropDown.onchange = goDD;
+		levelOfDelivery.addEventListener("change", showChanges);
 
-		function goDD() {
-			console.log(dropDown.value);
+		function showChanges() {
+			console.log(levelOfDelivery.value);
 		}
 
+		costOfDelivery = levelOfDelivery.value;
+
+		levelOfDelivery.addEventListener('submit', goDropDown);
+		function goDropDown() {
+			if (costOfDelivery === "0") {
+				alert("Please select a shipping method");
+				levelOfDelivery.style.backgroundColor = 'pink';
+				levelOfDelivery.focus();
+				return false;
+			} else {
+				shipInfo.cost = costOfDelivery;
+			}
+		}
 
 		/**************************
 		 	USER NAME VALIDATION
 		 ***************************/
 		//Variable created to store user 'Name'
 		var userName = formHandle.in_Name.value;
+
 		if (userName === '' || userName === null) {
-			var nameColor = document.getElementById('in_Name');
-			nameColor.style.backgroundColor = 'pink';
+			//Instruct user to enter name
 			alert("Please enter your name.");
+			//Capturing the name input field as a var
+			var nameColor = document.getElementById('in_Name');
+			//Set the background color of input field to pink
+			nameColor.style.backgroundColor = 'pink';
+			//Re-direct cursor focus back to this input field
+			nameColor.focus();
+			//Stop the form from sending
 			return false;
 		} else {
 			//Setting the 'name' property of the 'shipInfo' object to the 'userName'
 			shipInfo.name = userName;
-			console.log(userName);
 		}
-
 
 		/***************************
 			CLIENT ID VALIDATION
 		****************************/
 		//Variable to hold the value of the client id input
 		var client_id = formHandle.in_Id.value;
+		//Reg-ex for Client ID
 		var c_id_format = /\d\d\d\d\d\d/;
+
 		if(client_id === null || !c_id_format.test(client_id) || isNaN(client_id)) {
+			//Instructions alert
+			alert("Enter 6 digit number");
+			//Capturing the ID input field as a var
 			var id_color = document.getElementById('in_Id');
+			//Setting the input field to pink
 			id_color.style.backgroundColor = 'pink';
-			alert("Client ID must be 6 numbers 0-9");
+			//Stopping the form from sending
 			return false;
 		} else {
 			//Setting the 'cid' member to user data
@@ -97,6 +121,7 @@ function firePage() {
 		/************************************
 		 	SUCCESSFUL SUBMIT INSTRUCTIONS
 		 ************************************/
+
 		//Success message
 		alert("Form Submitted!");
 		//onSubmit, hide the form.
@@ -107,13 +132,12 @@ function firePage() {
 			THANK YOU MESSAGE
 		*************************/
 		//Hard coded thank you message
-		var thankYouMsg = `${shipInfo.name} (Customer #${shipInfo.cid}) for your purchase. Your package will be sent to '${shipInfo.pc}'`;
+		var thankYouMsg = `${shipInfo.name} (Customer #${shipInfo.cid}) for your purchase. Your package will be sent by  '${shipInfo.pc}'`;
 		//Holds the container to display the thank you message
 		var sendThanks = document.getElementById('thanksCustomer');
 		//Setting the contents of the container to the thank you message
 		sendThanks.innerHTML = thankYouMsg;
 		sendThanks.style.backgroundColor = 'greenyellow';
-
 
 		//Stop the form from sending
 		return false;
